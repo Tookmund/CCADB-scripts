6f8011a61cbd35e1898169db5a6040da1b272a8e
Jacob Adams <jacob@tookmund.com>
2024-05-19 17:02:30 -0400

    Update to new CCADB export format

--- ccadb.sh
+++ ccadb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-curl https://ccadb.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormat > AllCertRecords.csv
+curl https://ccadb.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormatv2 > AllCertRecords.csv
 curl https://ccadb.my.salesforce-sites.com/ccadb/AllIncludedRootCertsCSV > AllIncludedRootCerts.csv
 curl "https://ccadb.my.salesforce-sites.com/ccadb/AllCertificatePEMsCSVFormat?NotBeforeDecade=1990" > PEMs1990.csv
 curl "https://ccadb.my.salesforce-sites.com/ccadb/AllCertificatePEMsCSVFormat?NotBeforeDecade=2000" > PEMs2000.csv
